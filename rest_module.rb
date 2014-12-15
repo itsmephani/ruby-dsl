fcc8f6df9381ffba5e1798049c18f77be7fc66b3
phani <phani@heresy.in>
2014-12-15 18:54:14 +0530

    ..

--- rest_module.rb
+++ rest_module.rb
@@ -1,6 +1,13 @@
 module RestServiceModule
   class HelloService
     def self.index()
+      class_name = self.name.split('::').last
+      file = File.open("./simple_dsl/#{class_name.downcase}.rb", "w")
+      template = File.open("./simple_dsl/template.txt")
+      file.write("class #{class_name.capitalize} < B")
+      IO.copy_stream(template, file)
+      file.write("end")
+      file.close
       return JSON.generate({:data => 'Hello World'})
     end
  
